7122e90133e41db4a90e9f4371a8788ca54ad418
Adam Ovadia <adam@aovadia.com>
2024-05-28 19:14:19 -0400

    Add back lists projects

---
+++ Code Challenges/Lists/CombineSort.py
@@ -0,0 +1,6 @@
+#Write your function here
+
+def combine_sort(my_list1, my_list2): return sorted(my_list1 + my_list2)
+
+#Uncomment the line below when your function is done
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
---
+++ Code Challenges/Lists/MoreThanN.py
@@ -0,0 +1,6 @@
+#Write your function here
+
+def more_than_n(my_list, item, n): return my_list.count(item) > n
+
+#Uncomment the line below when your function is done
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
