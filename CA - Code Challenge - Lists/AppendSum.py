9e1f9276502b71fde93884af78994967785d4418
Adam Ovadia <adam@aovadia.com>
2024-05-28 18:05:55 -0400

    Reorganize programs

--- CA - Code Challenge - Lists/AppendSum.py
+++
@@ -1,7 +0,0 @@
-#Write your function here
-def append_sum(my_list):
-    for i in range(3):
-        my_list.append(my_list[-1] + my_list[-2])
-    return my_list
-#Uncomment the line below when your function is done
-print(append_sum([1, 1, 2]))
--- CA - Code Challenge - Lists/MoreThanN.py
+++
@@ -1,6 +0,0 @@
-#Write your function here
-
-def more_than_n(my_list, item, n): return my_list.count(item) > n
-
-#Uncomment the line below when your function is done
-print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
