2f1dd343ef61ce2a0da28d1b3737635462711548
Adam Ovadia <adam@aovadia.com>
2024-05-28 11:00:59 -0400

    Improve code by combining decrypt and encrypt functions

--- Coded Correspondence/Vigenère.py
+++ Coded Correspondence/Vigenère.py
@@ -2,56 +2,40 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
+# Generate a Vigenere keyword phrase based on the length of the entire message
 def keyword_phrase(keyword, message_length):
     phrase = ""
     for num in range(message_length):
         phrase += keyword[num % len(keyword)]
     return phrase
 
+# Return the position of a character in the alphabet
 def find_position_in_alphabet(word):
     return alphabet.find(word)
 
-def vig_decrypt(message, keyword):
+# Vigenere alg.
+# Inputs: message -> Type: Str -> text to encrypt or decrypt
+#         keyboard -> Type: Str -> key for Vigenerge
+#         decrypt -> Type Bool -> Default True to decrypt, False to Encrypt
+def vig(message, keyword, decrypt=True):
     keyphrase = keyword_phrase(keyword, len(message))
-    decrypted_message = ""
+    return_message = ""
     counter = 0
     for char in message:
         if char in alphabet:
-
-            #message_index = alphabet.find(char[0])
             message_index = find_position_in_alphabet(char)
             key_index = find_position_in_alphabet(keyphrase[counter])
+            if not decrypt: key_index = key_index * -1
             counter += 1
-            #decrypted_index = (message_index + key_index) % 26
-            #print("Message Index: {}, Key Index: {},  Decrypted_Index = {}".format(message_index, key_index, decrypted_index))
-            decrypted_message += alphabet[(message_index + key_index) % 26]
+            return_message += alphabet[(message_index + key_index) % 26]
         else:
+            return_message += char
+    return return_message
 
-            decrypted_message += char
-    return decrypted_message
 
-
-def vig_encrypt(message, keyword):
-    keyphrase = keyword_phrase(keyword, len(message))
-    encrypted_message = ""
-    counter = 0
-    for char in message:
-        if char in alphabet:
-
-            #message_index = alphabet.find(char[0])
-            message_index = find_position_in_alphabet(char)
-            key_index = find_position_in_alphabet(keyphrase[counter])
-            counter += 1
-            #decrypted_index = (message_index + key_index) % 26
-            #print("Message Index: {}, Key Index: {},  Decrypted_Index = {}".format(message_index, key_index, decrypted_index))
-            encrypted_message += alphabet[(message_index - key_index) % 26]
-        else:
-
-            encrypted_message += char
-    return encrypted_message
-
+# Codecademy test calls
 message = "txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztcgcexxch!"
 key = "friends"
 decrypted_message = "you were able to decode this? nice work! you are becoming quite the expert at crytography!"
-print(vig_decrypt(message.lower(), key))
-print(vig_encrypt(decrypted_message, key))
+print(vig(decrypted_message.lower(), key, False))
+print(vig(message.lower(), key))
